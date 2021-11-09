06a5dd1c63c39b1b0a7f3539161272599b72e95f
shuklasan78 <32901685+shuklasan78@users.noreply.github.com>
2021-11-09 19:01:37 +0100

    Create SalesVO.java

---
+++ src/main/java/data/SalesVO.java
@@ -0,0 +1,23 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data @NoArgsConstructor
+public class SalesVO {
+
+    private  String region = "" ;
+    private  String country = "";
+    private  String itemType = "";
+    private  String salesChannel = "";
+    private  String orderPriority = "";
+    private  String orderDate = null;
+    private  Integer orderID = 0;
+    private  String shipDate = null ;
+    private  Integer unitsSold = 0;
+    private  Double unitPrice = 0.0;
+    private  Double unitCost =  0.0;
+    private  Double totalRevenue = 0.0;
+    private  Double totalCost = 0.0;
+    private  Double totalProfit = 0.0;
+}
