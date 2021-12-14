bf37fc60f8a1ed68829ad0dc0cbcbb8f5ffdb276
sandeepkumarshukla <sandeep.shukla@gmail.com>
2021-12-14 12:37:22 +0100

    Arrays sorting login in sperate class

--- src/main/java/collection/arrays/ArraysAdvancedStream.java
+++ src/main/java/collection/arrays/ArraysAdvancedStream.java
@@ -18,7 +18,6 @@ public class ArraysAdvancedStream {
     public static void main(String[] args) {
         //convertArraysToCollections();
         //removeDuplicatesFromArrays();
-        sortingArrays();
         //mergingArrays();
     }
 
@@ -51,43 +50,6 @@ public class ArraysAdvancedStream {
                 .stream(salesArr)
                 .collect(Collectors.groupingBy(p->p.getOrderID(),Collectors.toList()));
         log.info("The size of the Map after grouping :"+salesMap.size());
-
-    }
-
-    private static void sortingArrays() {
-        SalesVO[] salesArr = GetSalesData.getSalesArray(FilesEnum.SalesRecords2M.toString());
-        sortArrays(salesArr);
-    }
-
-    private static void sortArrays(SalesVO[] salesArr) {
-        long start = new Date().getTime();
-        SalesVO[] newArray1 = salesArr;
-        Arrays.sort(newArray1, Comparator.comparing(SalesVO::getOrderID));
-        Arrays.sort(newArray1, Comparator.comparing(p -> p.getOrderID()));
-        long end = new Date().getTime();
-        log.info("Time taken :" +(end-start));
-        System.out.println("First Value -1 :"+newArray1[0].getOrderID()+"    Last Value-1 :"+newArray1[newArray1.length-1].getOrderID());
-
-        SalesVO[] newArray2 = salesArr;
-        // Functions for getting first and last names from an Employee
-        Function<SalesVO, Integer> funcbyOrderId = SalesVO::getOrderID;
-        Function<SalesVO, String> functbyCountry = SalesVO::getCountry;
-        // Comparator for comparing Sales by OrderId  then country
-        Comparator<SalesVO> comparestr = Comparator.comparing(funcbyOrderId).thenComparing(functbyCountry);
-        // sort employees in descending order by last name, then first name
-        Arrays.stream(newArray2).sorted(comparestr.reversed());
-        System.out.println("First Value -2 :"+newArray2[0].getOrderID()+"    Last Value-2 :"+newArray2[newArray2.length-1].getOrderID());
-
-        SalesVO[] newArray3 = salesArr ;
-        Comparator<SalesVO> compareByRevenue = (SalesVO o1, SalesVO o2) -> o1.getTotalRevenue().compareTo( o2.getTotalRevenue() );
-        Arrays.stream(newArray3).sorted(compareByRevenue);
-        System.out.println("First Value- 3 :"+newArray3[0].getOrderID()+"    Last Value - 3 :"+newArray3[newArray3.length-1].getOrderID());
-        //sorting arrays in decending order
-        SalesVO[] newArray4 = salesArr ;
-        Arrays.sort(newArray4, Collections.reverseOrder(Comparator.comparing(funcbyOrderId)));
-        System.out.println("First Value- 4 :"+newArray4[0].getOrderID()+"    Last Value - 4 :"+newArray4[newArray4.length-1].getOrderID());
-        //using list it is simple
-        //Collections.sort(Arrays.stream(newArray4).toList(), compareByRevenue);
 
     }
 
---
+++ src/main/java/collection/arrays/SortingArrays.java
@@ -0,0 +1,76 @@
+package collection.arrays;
+
+import data.FilesEnum;
+import data.GetSalesData;
+import data.SalesVO;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.function.Function;
+
+@Slf4j
+public class SortingArrays {
+
+    public static void main(String[] args) {
+        sortArrays();
+    }
+
+    private static void sortArrays() {
+        String fileName = FilesEnum.SalesRecords2M.toString();
+        sortArraysNaturalOrder(fileName);
+        sortArraysNaturalOrderWithFunction(fileName);
+        sortArrayNaturalOrderUisngComparator(fileName);
+        sortArrayReverse(fileName);
+    }
+    private static void sortArraysNaturalOrder(String fileName) {
+        SalesVO[] salesArr = GetSalesData.getSalesArray(fileName);
+        long start = new Date().getTime();
+        SalesVO[] newArray1 = salesArr;
+        Arrays.sort(newArray1, Comparator.comparing(SalesVO::getOrderID));
+        Arrays.sort(newArray1, Comparator.comparing(p -> p.getOrderID()));
+        long end = new Date().getTime();
+        log.info("Time taken :" +(end-start));
+        System.out.println("First Value -1 :"+newArray1[0].getOrderID()+"    Last Value-1 :"+newArray1[newArray1.length-1].getOrderID());
+    }
+
+    private static void sortArraysNaturalOrderWithFunction(String fileName) {
+        SalesVO[] salesArr = GetSalesData.getSalesArray(fileName);
+        long start = new Date().getTime();
+        // Functions for getting first and last names from an Employee
+        Function<SalesVO, Integer> funcbyOrderId = SalesVO::getOrderID;
+        Function<SalesVO, String> functbyCountry = SalesVO::getCountry;
+        // Comparator for comparing Sales by OrderId  then country
+        Comparator<SalesVO> comparestr = Comparator.comparing(funcbyOrderId).thenComparing(functbyCountry);
+        // sort employees in descending order by last name, then first name
+        Arrays.stream(salesArr).sorted(comparestr);
+        long end = new Date().getTime();
+        log.info("Time taken :" +(end-start));
+        System.out.println("First Value -2 :"+salesArr[0].getOrderID()+"    Last Value-2 :"+salesArr[salesArr.length-1].getOrderID());
+
+
+    }
+    //This is best performance
+    private static void sortArrayNaturalOrderUisngComparator(String fileName) {
+        SalesVO[] salesArr = GetSalesData.getSalesArray(fileName);
+        long start = new Date().getTime();
+        Comparator<SalesVO> compareByRevenue = (SalesVO o1, SalesVO o2) -> o1.getTotalRevenue().compareTo( o2.getTotalRevenue() );
+        Arrays.stream(salesArr).sorted(compareByRevenue);
+        long end = new Date().getTime();
+        log.info("Time taken :" +(end-start));
+        System.out.println("First Value- 3 :"+salesArr[0].getOrderID()+"    Last Value - 3 :"+salesArr[salesArr.length-1].getOrderID());
+
+    }
+
+    private static void sortArrayReverse(String fileName) {
+        SalesVO[] salesArr = GetSalesData.getSalesArray(fileName);
+        long start = new Date().getTime();
+        Function<SalesVO, Integer> funcbyOrderId = SalesVO::getOrderID;
+        Arrays.sort(salesArr, Collections.reverseOrder(Comparator.comparing(funcbyOrderId)));
+        long end = new Date().getTime();
+        log.info("Time taken :" +(end-start));
+        System.out.println("First Value- 4 :"+salesArr[0].getOrderID()+"    Last Value - 4 :"+salesArr[salesArr.length-1].getOrderID());
+    }
+}
